e8f25065c0b1f8dc0978e7bf7e9d5a240858e3f9
sujan-rai <sujanrai8448@gmail.com>
2025-06-09 14:36:35 +0545

    modification
    modification

--- buildVars.py
+++ buildVars.py
@@ -16,29 +16,30 @@ def _(arg):
 # Add-on information variables
 addon_info = {
 	# add-on Name/identifier, internal for NVDA
-	"addon_name": "addonTemplate",
+	"addon_name": "browser history remover",
 	# Add-on summary/title, usually the user visible name of the add-on
 	# Translators: Summary/title for this add-on
 	# to be shown on installation and add-on information found in add-on store
-	"addon_summary": _("Add-on user visible name"),
+	"addon_summary": _("browser history remover"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
-	"addon_description": _("""Description for the add-on.
-It can span multiple lines."""),
+	"addon_description": _("""a powerful nvda add-on to remove multiple browser's history in one click.
+to access this add-on, press nvda+alt+b or go to nvda-menu>tools-menu and open it.
+developed by: sujan rai at tech visionary team."""),
 	# version
-	"addon_version": "x.y",
+	"addon_version": "0.1",
 	# Author(s)
-	"addon_author": "name <name@domain.com>",
+	"addon_author": "sujan rai <sujanrai8448@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url": None,
+	"addon_url": "https://t.me/techvisionary",
 	# URL for the add-on repository where the source code can be found
 	"addon_sourceURL": None,
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": None,
+	"addon_minimumNVDAVersion": 2023.1,
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": None,
+	"addon_lastTestedNVDAVersion": 2025.1,
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
@@ -57,7 +58,7 @@ It can span multiple lines."""),
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = []
+pythonSources = ["addon/globalPlugins/browserHistoryRemover/*.py"]
 
 # Files that contain strings for translation. Usually your python sources
 i18nSources = pythonSources + ["buildVars.py"]
